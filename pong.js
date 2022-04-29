0cee548ecd43772ae29562208ba4672a26f70ae5
Arturo Palafox <arturopalafox3500@gmail.com>
2022-04-28 20:58:40 -0700

    commit to save to github

---
+++ pong.js
@@ -0,0 +1,6 @@
+// select canvas element
+const canvas = document.getElementById("pong");
+
+// getContext of canvas = methods and properties to draw and do a lot of thing to the canvas
+const ctx = canvas.getContext('2d');
+
